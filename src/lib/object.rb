0d03d9b41f2404c8d8e7747a53e94112d187c81c
Alejandro <aleperaltabazas@hotmail.com>
2018-12-30 00:35:06 -0300

    removing accessors from the method attributes, removing class Attributes

--- src/lib/object.rb
+++ src/lib/object.rb
@@ -1,37 +1,16 @@
-require_relative 'attributes'
-
 class Object
-  private
-
-  attr_accessor :attrs
-
-  public
-
   def self.attr_reader(*vars)
-    self.attrs = Attributes.new if attrs.nil?
-
-    attrs.add_readers(vars)
-    super(*vars)
-  end
-
-  def self.attr_accessor(*vars)
-    self.attrs = Attributes.new if attrs.nil?
-
-    attrs.add_accessors(vars)
+    @attributes ||= []
+    @attributes.concat vars
     super(*vars)
   end
 
   def self.attributes
-    if attrs.nil?
-      []
-    else
-      attrs.all_attributes.flatten
-    end
+    @attributes ||= []
+    @attributes
   end
 
   def attributes
     self.class.attributes
   end
-
-  private
 end
--- src/spec/attributes_spec.rb
+++ src/spec/attributes_spec.rb
@@ -2,7 +2,6 @@ require 'rspec'
 require_relative '../lib/object'
 
 describe 'Attributes spec' do
-
   it 'Defining only readers should still give me a list of the attributes' do
     class A
       attr_reader :a, :b
@@ -12,23 +11,22 @@ describe 'Attributes spec' do
     expect(a.attributes).to eq(%i[a b])
   end
 
-  it 'Defining only accessors should still give me a list of the attributes' do
+  it 'Defining only accessors should give me an empty list' do
     class B
       attr_accessor :a, :b
     end
 
     b = B.new
-    expect(b.attributes).to eq(%i[a b])
+    expect(b.attributes).to eq([])
   end
 
-  it 'Defining both accessors and readers should give me a list with readers ahead of accessors' do
+  it 'Defining both accessors and readers should give me a list with the readers' do
     class C
       attr_accessor :a, :b
       attr_reader :c, :d
     end
 
     c = C.new
-    expect(c.attributes).to eq(%i[c d a b])
+    expect(c.attributes).to eq(%i[c d])
   end
-
 end
